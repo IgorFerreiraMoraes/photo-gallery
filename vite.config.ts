fa93a5b18129764093a1abe98897b0bdba691326
Igor Ferreira Moraes <igorfm13524@gmail.com>
2023-09-24 17:26:31 -0300

    Adiciona manifesto

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,36 @@ import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwa_manifest: Partial<VitePWAOptions> = {
+	registerType: 'prompt',
+	includeAssets: ['favicon.png'],
+	manifest: {
+		name: 'Photo Gallery',
+		short_name: 'Photo Gallery',
+		description: 'Vue & Ionic Test PWA',
+		icons: [
+			{
+				src: '/favicon.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'any maskable',
+			},
+		],
+		theme_color: '#3880ff',
+		background_color: '#fffffff',
+		display: 'standalone',
+		scope: '/',
+		start_url: '/',
+		orientation: 'portrait',
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './',
-	plugins: [vue(), legacy(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [vue(), legacy(), VitePWA(pwa_manifest)],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
