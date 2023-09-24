ec988ce6c870dd7a5cd60bffdf3e4551082145f5
Igor Ferreira Moraes <igorfm13524@gmail.com>
2023-09-24 16:37:05 -0300

    Define a URL base como o link da Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
+	base: 'https://photo-gallery-neon-five.vercel.app/',
 	plugins: [vue(), legacy(), VitePWA({ registerType: 'autoUpdate' })],
 	resolve: {
 		alias: {
